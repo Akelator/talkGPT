20fca6c61e8330b7092d9b88539073e10376c8ca
Adrian Termenon <adrianriera@gmail.com>
2023-04-03 20:28:20 +0200

    clean css

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -11,7 +11,7 @@ import { AvailabilityService, ChromeService, Speech2TextService, SubSink, Text2S
 export class ContentComponent implements AfterViewInit, OnDestroy {
   private _sub = new SubSink();
 
-  isChat: boolean = false;
+  isChat: boolean = true;
   response: string = "";
   recording: boolean = false;
   writting: boolean = true;
